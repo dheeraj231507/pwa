cbf0f4c723335e7d88d55fbb5f4de81e96da15ae
Dheeraj kumawat <dheerajkumawat0707@gmail.com>
2025-02-12 15:01:00 +0530

    change firebase project

--- public/helper/firebase.js
+++ public/helper/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1eR66a4wzSB-4s3EFs5w7dgpv8Ryxarw",
-  authDomain: "oneup-f71f5.firebaseapp.com",
-  projectId: "oneup-f71f5",
-  storageBucket: "oneup-f71f5.firebasestorage.app",
-  messagingSenderId: "560676854221",
-  appId: "1:560676854221:web:c13786026853a1e2660c84",
-  measurementId: "G-E8Z8TLLNYG",
+  apiKey: "AIzaSyDTUuvkjg6pJrxpW-7L7uOTH_fHqY9Id08",
+  authDomain: "oneup1-402f5.firebaseapp.com",
+  projectId: "oneup1-402f5",
+  storageBucket: "oneup1-402f5.firebasestorage.app",
+  messagingSenderId: "636618535187",
+  appId: "1:636618535187:web:0ac9a5eebea78f561d2ae6",
+  measurementId: "G-NT2JZ2TZCS",
 };
 
 export const app = initializeApp(firebaseConfig);
